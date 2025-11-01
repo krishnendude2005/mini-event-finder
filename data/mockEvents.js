c2a5c63229afb18111a3ce40f3cef5d36fac1536
Krishnendu De <krishnendu1205.be22@chitkarauniversity.edu.in>
2025-11-01 19:39:42 +0530

    Update mockEvents.js

--- data/mockEvents.js
+++ data/mockEvents.js
@@ -4,21 +4,26 @@ module.exports = [
     title: "Conference",
     date: "2025-11-10",
     location: "New York",
-    description: "Annual tech conference"
+    description: "Annual tech conference",
+    maxParticipants: 100,
+    currentParticipants: 45
   },
   {
     id: "2",
     title: "Meetup",
     date: "2025-11-15",
     location: "San Francisco",
-    description: "Community meetup"
+    description: "Community meetup",
+    maxParticipants: 50,
+    currentParticipants: 30
   },
   {
     id: "3",
     title: "Webinar",
     date: "2025-12-01",
     location: "Online",
-    description: "Frontend performance best practices"
+    description: "Frontend performance best practices",
+    maxParticipants: 200,
+    currentParticipants: 150
   }
 ];
-
